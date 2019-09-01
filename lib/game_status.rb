af6d728b89f888fac596d442a68202a6dee1c3c5
Clark Chen <yyunqingchenn@gmail.com>
2019-09-01 00:01:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,21 +28,24 @@ WIN_COMBINATIONS = [
   ]
   
 def won?(board)
-  WIN_COMBINATIONS.each do |i|
-    if board[i] == "X"
-      return i
-    else
-      return false 
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+    
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
-  end
-end 
-  
-def full?(board)
-    [0,1,2,3,4,5,6,7,8].each do |index|
-    if board[index] == "X" || board[index] == "O"
-      return true 
-    else
-      return false 
-    end 
-  end 
-end
+  }
+   return false 
+ end 
+ 
+ def full?(board)
+   board.all? {|index| index == "X" || index == "O"}
+ end 
